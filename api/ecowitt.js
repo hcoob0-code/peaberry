cf3ac9fca7ff72cd5b15bd879956a26cb0806b65
hcoob0-code <hcoob0@gmail.com>
2026-05-03 18:42:24 +0900

    Update ecowitt.js

--- api/ecowitt.js
+++ api/ecowitt.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
   if (req.method === 'OPTIONS') { res.status(200).end(); return; }
   const { application_key, api_key, mac } = req.query;
-  const url = `https://api.ecowitt.net/api/v3/device/real_time?application_key=${application_key}&api_key=${api_key}&mac=${mac}&call_back=all&temp_unitid=1&pressure_unitid=3&wind_speed_unitid=6&rainfall_unitid=12&solar_irradiance_unitid=17`;
+  const url = `https://api.ecowitt.net/api/v3/device/real_time?application_key=${application_key}&api_key=${api_key}&mac=${mac}&call_back=all&temp_unitid=1&pressure_unitid=3&wind_speed_unitid=6&rainfall_unitid=12&solar_irradiance_unitid=16`;
   const r = await fetch(url);
   const data = await r.json();
   res.status(200).json(data);
